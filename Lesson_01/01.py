b17008ace9749245265b1c55b836e3dec19ea900
Vitalii Kravchuk <workspaceanyplace@gmail.com>
2023-08-10 18:48:55 +0300

    Commit_02_frresh_start

---
+++ Lesson_01/01.py
@@ -0,0 +1,23 @@
+class A:
+  def foo(self):
+    print("Foo from A")
+
+
+class B(A):
+  def foo(self):
+    super().foo()
+
+
+class C(A):
+  def foo(self):
+    print("Foo from C")
+
+
+class D(B, C):
+  def foo(self):
+    super(A, self).foo()
+
+
+instance = D()
+print(D.__mro__)
+instance.foo
