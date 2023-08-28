d3d3cf190f9539c6efd5bf5c4150c311c78087f2
Vitalii Kravchuk <workspaceanyplace@gmail.com>
2023-08-28 07:00:06 +0300

    lesson_03 homework formated

--- Lesson_03/04_homework.py
+++ Lesson_03/04_homework.py
@@ -7,6 +7,7 @@ lines: str = file.readlines()
 
 word = "user"
 
+
 def add_all_findings(file, word):
     result_list = []
     for line in lines:
@@ -22,7 +23,10 @@ def file_word_checker(file, word):
     for line in lines:
         if word in line:
             while True:
-                users_input = input(f"Found line: '{line}'. Add to result? (y/n) or type 'break' to close the program: ")
+                users_input = input(
+                    f"Found line: '{line}'. \
+                Add to result? (y/n) or type 'break' to close the program: "
+                )
                 if users_input == "y":
                     result.append(line)
                     print(f"{line=} has been added to {len(result)=}.")
@@ -33,20 +37,18 @@ def file_word_checker(file, word):
                     break
                 else:
                     print("Invalid input")
-                    users_input = input(f"Found line: '{line}'. Add to result? (y/n) or type 'break' to close the program: ")
+                    users_input = input(
+                        f"Found line: '{line}'. \
+                Add to result? (y/n) or type 'break' to close the program: "
+                    )
             if users_input == "break":
                 break
     return len(result)
 
+
 add_all_result = add_all_findings(file, word)
 print(add_all_result)
 
+
 word_checker_result = file_word_checker(file, word)
 print(word_checker_result)
-
-
-
-
-
-
-                
