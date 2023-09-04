e7f771789fb4b5158392ce27968f334e0a35ba87
Vitalii Kravchuk <workspaceanyplace@gmail.com>
2023-09-04 17:36:39 +0300

    03_random_uuid has been formated by black .

--- Lesson_04/03_random_uuid.py
+++ Lesson_04/03_random_uuid.py
@@ -11,19 +11,20 @@ from uuid import UUID, uuid4
 #       if generated_uuid not in used_uuid_set:
 #           used_uuid_set.add(generated_uuid)
 #           return generated_uuid
-      
+
 
 def uuid_from_generator() -> Generator[UUID, None, None]:
-   used_uuid_set: set[UUID] = set()
-   while True:
-      generated_uuid = uuid4()
-      if generated_uuid not in used_uuid_set:
-         used_uuid_set.add(generated_uuid)
-         yield generated_uuid
+    used_uuid_set: set[UUID] = set()
+    while True:
+        generated_uuid = uuid4()
+        if generated_uuid not in used_uuid_set:
+            used_uuid_set.add(generated_uuid)
+            yield generated_uuid
+
 
 uuid = uuid_from_generator()
-      
-      
+
+
 print(next(uuid))
 print(next(uuid))
 print(next(uuid))
@@ -36,13 +37,15 @@ used_uuid_dict_by_user: dict[str, set[UUID]] = {}
 #    "Marry": {5abc5dba-e45c-4782-8074-7abb55db01d3, ...}
 # }
 
+
 def generate_uuid_from_function(user: str) -> UUID:
     while True:
-      generated_uuid = uuid4()
-      if generated_uuid not in used_uuid_dict_by_user[user]:
-          used_uuid_dict_by_user[user].add(generated_uuid)
-          return generated_uuid
-      
+        generated_uuid = uuid4()
+        if generated_uuid not in used_uuid_dict_by_user[user]:
+            used_uuid_dict_by_user[user].add(generated_uuid)
+            return generated_uuid
+
+
 john_uuid = uuid_from_generator()
 marry_uuid = uuid_from_generator()
 
