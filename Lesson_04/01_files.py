9bd3f861475196deff6eb6f15fd086200c5bd2b5
Vitalii Kravchuk <workspaceanyplace@gmail.com>
2023-09-04 17:35:59 +0300

    01_files has been formated by black .

--- Lesson_04/01_files.py
+++ Lesson_04/01_files.py
@@ -6,14 +6,16 @@ ROOT_DIR: Path = Path(__file__).absolute().parent.parent
 # lines: list[str] = file.readlines()
 result: list[str] = []
 
+
 def get_file_lines(filename: Path):
     file = open(filename)
     while True:
         line = file.readline()
         if not line:
             break
-        
+
         yield line
+
 
 for line in get_file_lines(ROOT_DIR / "rockyou.txt", encoding="utf-8"):
     user_input = input("Do you want to add the line: {line}?")
