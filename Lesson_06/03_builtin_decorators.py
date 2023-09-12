ea2445ac0ce9fcf2ede0a0174f6d760bde2b401b
Vitalii Kravchuk <workspaceanyplace@gmail.com>
2023-09-12 15:16:37 +0300

    03_builtin_decorators issues has been fixed

--- Lesson_06/03_builtin_decorators.py
+++ Lesson_06/03_builtin_decorators.py
@@ -1,63 +1,61 @@
 import time
 from typing import Callable
 
+
 class Person:
-	def __init__(self, name: str, age: int) -> None:
-		self.name: str = name
-		self.age: int = age
+    def __init__(self, name: str, age: int) -> None:
+        self.name: str = name
+        self.age: int = age
 
-	def perf_counter(func: callable):
-		def inner(*args, **kwargs):
-			start = time.perf_counter()
-			result = func(*args, **kwargs)
-			exec_time = time.perf_counter() - start
-			print(f"Exec time of: {func.__name__} is {exec_time}")
-			return result
-		return inner
+    def perf_counter(func: Callable):
+        def inner(*args, **kwargs):
+            start = time.perf_counter()
+            result = func(*args, **kwargs)
+            exec_time = time.perf_counter() - start
+            print(f"Exec time of: {func.__name__} is {exec_time}")
+            return result
 
-	@perf_counter
-	def person_greetings(self):
-		print(f"{self.name} say's: 'Hello!' to naked ladies!")
-		return f"That's message from {self.name}."
-	
-	@staticmethod
-	def person_goodbye(name: str):
-		print(f"{name} say's: 'See you tomorrow!'")
+        return inner
 
-	@property
-	def is_adult(self) -> bool:
-		if self.age < 18:
-			return False
-		else:
-			return True
-		
+    @perf_counter
+    def person_greetings(self):
+        print(f"{self.name} say's: 'Hello!' to naked ladies!")
+        return f"That's message from {self.name}."
 
+    @staticmethod
+    def person_goodbye(name: str):
+        print(f"{name} say's: 'See you tomorrow!'")
 
+    @property
+    def is_adult(self) -> bool:
+        if self.age < 18:
+            return False
+        else:
+            return True
 
 
 class NotPerson:
-	_instance = None
-	_initialized: bool = False
+    _instance = None
+    _initialized: bool = False
 
-	def __new__(cls, *args, **kwargs):
-		if not cls._instance:
-			cls._instance = super().__new__(cls)
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
 
-		return cls._instance
-	
+        return cls._instance
 
-	def __init__(self, name: str, age: int) -> None:
-		if self._initialized:
-			return
-		
-		self.name: str = name
-		self.age: int = age
+    def __init__(self, name: str, age: int) -> None:
+        if self._initialized:
+            return
 
-		self._initialized = True
+        self.name: str = name
+        self.age: int = age
 
-	@classmethod
-	def notperson_greetings(cls):
-		print(f"{cls._instance} say's: 'Hello to eveyone!'")
+        self._initialized = True
+
+    @classmethod
+    def notperson_greetings(cls):
+        print(f"{cls._instance} say's: 'Hello to eveyone!'")
 
 
 person_John = Person(name="John", age=19)
