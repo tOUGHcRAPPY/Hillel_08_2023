c8477bc3ca3b5736d9b46d2a9e7194aa5abac21b
Vitalii Kravchuk <workspaceanyplace@gmail.com>
2023-10-06 20:51:45 +0300

    07_homework has been updated

--- Lesson_09/07_homework.py
+++ Lesson_09/07_homework.py
@@ -6,8 +6,7 @@ from datetime import datetime
 
 
 class SocialChannel:
-    def __init__(self, channel: str, followers: int):
-        self.channel: str = channel
+    def __init__(self, followers: int):
         self.followers: int = followers
 
 
@@ -33,45 +32,33 @@ class Twitter(SocialChannel):
 
 # each post has a message and the timestamp when it should be posted
 class Post:
+    def __init__(self, message: str, channel: SocialChannel) -> None:
+        self.message = message
+        self.channel = channel
+
     def process_schedule(self, year, month, day, hour, minute: int):
         self.scheduled_time = datetime(year, month, day, hour, minute)
         print(self.scheduled_time)
         return self.scheduled_time
 
     def post_a_message(
-        self, channel: SocialChannel, message: str, scheduled_time: int
+        self
     ):
         current_time = datetime.now()
-        time_difference = scheduled_time - current_time
+        time_difference = self.scheduled_time - current_time
         time_in_seconds = time_difference.total_seconds()
-        time.sleep(time_in_seconds)
-        if isinstance(channel, Youtube):
-            greeting = channel.greeting_message()
-            print(f"{greeting}\n{message}\nPosted at:{current_time}.")
-        elif isinstance(channel, Facebook):
-            greeting = channel.greeting_message()
-            print(f"{greeting}\n{message}\nPosted at:{current_time}.")
-        elif isinstance(channel, Twitter):
-            greeting = channel.greeting_message()
-            print(f"{greeting}\n{message}\nPosted at:{current_time}.")
-        else:
-            print("nothing to share...")
+        if time_in_seconds > 0:
+            time.sleep(time_in_seconds)
+        greeting = self.channel.greeting_message()
+        print(f"{greeting}\n{self.message}\nPosted at: {current_time}.")
 
 
-youtube_channel = Youtube("Youtube", 1000)
-facebook_channel = Facebook("Facebook", 100)
-twitter_channel = Twitter("Twitter", 10000)
+youtube_channel = Youtube(followers=1000)
+facebook_channel = Facebook(followers=100)
+twitter_channel = Twitter(followers=10000)
 
-social_post = Post()
+social_post_1 = Post(message="Help me...", channel=twitter_channel)
 
-social_post.process_schedule(2023, 10, 4, 21, 55)
+social_post_1.process_schedule(2023, 10, 6, 20, 50)
 
-# social_post.post_a_message(
-#     youtube_channel, "Hello, World!", social_post.scheduled_time
-# )
-# social_post.post_a_message(
-#     twitter_channel, "Hello, World!", social_post.scheduled_time
-# )
-social_post.post_a_message(
-    facebook_channel, "Hello, World!", social_post.scheduled_time
-)
+social_post_1.post_a_message()
