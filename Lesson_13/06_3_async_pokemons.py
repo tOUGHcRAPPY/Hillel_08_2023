e2b6152be8b3b6f940eaf35fe040ea388d46036a
Vitalii Kravchuk <workspaceanyplace@gmail.com>
2023-11-16 11:27:32 +0200

    06_3_async_pokemons has been added to Lesson_13

---
+++ Lesson_13/06_3_async_pokemons.py
@@ -0,0 +1,35 @@
+import asyncio
+import httpx
+import sys
+from typing import Coroutine
+from time import perf_counter
+
+
+BASE_URL = "https://pokeapi.co/api/v2/pokemon/{id_}"
+
+
+async def async_process(url: str):
+    async with httpx.AsyncClient() as client:
+        await client.get(url)
+
+
+async def _run(*tasks: Coroutine):
+    start_time = perf_counter()
+    await asyncio.gather(*tasks)
+    end_time = perf_counter()
+    total_time = end_time - start_time
+    print(total_time)
+
+
+def main():
+    num = int(sys.argv[2])
+    urls: list[str] = [BASE_URL.format(id_=i) for i in range(1, num + 1)]
+    if sys.argv[1] == "async":
+        tasks: list[Coroutine] = [async_process(url) for url in urls]
+        asyncio.run(_run(*tasks))
+    else:
+        raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
